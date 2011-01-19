05749c4d9d07c089b64daf3f01c27e22a16a36b6
Damon Maneice <damaneice@gmail.com>
2011-01-18 23:38:45 -0600

    created methods for creating a url and extracting a sunrise time

--- lib/sunrise.rb
+++ lib/sunrise.rb
@@ -1,16 +1,38 @@
 require 'net/http'
-
-require 'uri' require 'rexml/document'
+require 'uri' 
+require 'rexml/document'
 
 class Sunrise
     
-def initialize
 
-end
-xml = Net::HTTP.get(URI.parse('http://www.earthtools.org/sun/40.71417/-74.00639/4/12/-5/0'))
+  
+  def createURL(latitude, longitude)
+     'http://www.earthtools.org/sun/'+ latitude + '/'+ longitude + '/' + Time.now.day.to_s + '/' + 
+      Time.now.month.to_s + '/99/1'
+  end
 
-doc = REXML::Document.new(xml)
 
-doc.elements.each('sun/morning/sunrise') do |ele| puts ele.text end
+  def getXMLData(latitude, longitude)
+      url = createURL(latitude,longitude)
+      xml = Net::HTTP.get(URI.parse(url))
+  end
+  
+  def getSunriseTime(latitude,longitude)
+    xml = getXMLData(latitude,longitude)
+    extractSunriseTime(xml)
+  end  
+  
+  def extractSunriseTime(xml)
+    sunrise = ''
+    doc = REXML::Document.new(xml)
+    doc.elements.each('sun/morning/sunrise') do 
+      |ele|  sunrise = ele.text 
+    end
+    return sunrise
+  end
+
+
+
+
 
 end
--- spec/sunrise_spec.rb
+++ spec/sunrise_spec.rb
@@ -3,14 +3,50 @@ require 'sunrise'
 
 describe Sunrise do
   before(:each) do
-
+    @sunrise = Sunrise.new
   end
 
-  it "should" do
-
+  it "should create URL" do
+    @sunrise.createURL("42.48", "83.24").should == "http://www.earthtools.org/sun/42.48/83.24/18/1/99/1"
   end
-
-
   
+  
+  it "should extract sunrise time from xml" do
+    
+  @sunrise.extractSunriseTime(createSampleXML).should == '10:54:07'
+  end  
+  
+  def createSampleXML 
+  '<?xml version="1.0" encoding="ISO-8859-1" ?>
+  <sun xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.earthtools.org/sun.xsd">
+    <version>1.0</version>
+    <location>
+      <latitude>42.48</latitude>
+      <longitude>83.24</longitude>
+    </location>
+    <date>
+      <day>18</day>
+      <month>1</month>
+      <timezone>8</timezone>
+      <dst>1</dst>
+    </date>
+    <morning>
+      <sunrise>10:54:07</sunrise>
+      <twilight>
+        <civil>10:23:05</civil>
+        <nautical>09:48:26</nautical>
+        <astronomical>09:14:47</astronomical>
+      </twilight>
+    </morning>
+    <evening>
+      <sunset>20:21:41</sunset>
+      <twilight>
+        <civil>20:52:43</civil>
+        <nautical>21:27:23</nautical>
+        <astronomical>22:01:03</astronomical>
+      </twilight>
+    </evening>
+  </sun>'
+  end
 end
 
